0ddc3169c581769920ddffe8f80383c1aede1039
yuzutyaso <f.kokori85@gmail.com>
2025-08-23 07:43:40 +0900

    Add files via upload

---
+++ ai.js
@@ -0,0 +1,11 @@
+  if (body.includes("和歌")) {
+    await wakameAI.chatwithAI(roomId);
+  }
+  
+  const randomValue = Math.random();
+  if (randomValue < 0.5) {
+    await wakameAI.chatwithAI(roomId);
+  }
+
+
+800001
