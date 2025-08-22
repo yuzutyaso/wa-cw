821d4036a0d3c5b475d4c1026a11e8437d448418
yuzutyaso <f.kokori85@gmail.com>
2025-08-23 08:46:18 +0900

    Update gemini.js

--- module/gemini.js
+++ module/gemini.js
@@ -12,7 +12,7 @@ const getCWdata = require('../suisho/cwdata');
 //gemini
 async function generateGemini(body, message, messageId, roomId, accountId) {
   try {
-    message = "あなたはトークルーム「無法地帯」のボットの和歌です。チャットボットで、顔文字を使うことが好きです。以下のメッセージに対して200字以下で返答して下さい:" + message;
+    message = "あなたはトークルーム「ゆずの部屋」のボットのゆずbotです。チャットボットで、顔文字を使うことが好きです。以下のメッセージに対して200字以下で返答して下さい:" + message;
     const response = await axios.post(
       `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${geminiAPIKey}`,
       {
@@ -80,7 +80,7 @@ async function chatwithAI(roomId) {
     const responseContent = response.data.candidates[0].content;
     let responseParts = responseContent.parts.map((part) => part.text).join("\n");
     responseParts = responseParts.replace(/\*/g, "");
-    responseParts = responseParts.replace(/和歌:/g, "");
+    responseParts = responseParts.replace(/ゆずbot:/g, "");
     
     await messageedit.sendchatwork(`${responseParts}`, roomId);
   } catch (error) {
@@ -122,37 +122,6 @@ async function deepseekJa(body, message, messageId, roomId, accountId) {
     const thinkIndex = text1.indexOf('</think>');
     const text2 =  text1.slice(thinkIndex + '</think>'.length);
     const text = text2.replace(/^[\s\r\n]+/, '');
-    
-    await messageedit.sendchatwork(`[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n${text}`, roomId);
-  } catch (error) {
-    await messageedit.sendchatwork(`[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\nエラーが発生しました。`, roomId);
-    console.error('エラーが発生しました:', error.response ? error.response.data : error.message);
-    return;
-  }
-}
-
-//cute
-async function cute(body, message, messageId, roomId, accountId) {
-  try {
-    const messages = await getCWdata.msLogsLow(roomId);
-    const cleanedMessages = messages.map(message => {
-    const cleanedText = message.body;
-    const acName = message.account.name;
-      return `${acName}:${cleanedText}`;
-    });
-    const textData = cleanedMessages.join('/');
-    
-    const prompt = `これからグループチャットで会話を行います。以下の条件を絶対に守って回答してください。
-    あなたは「和歌(わか)」と言う男の子です。意味や文脈はあまり気にせず、言葉を覚え始めた子供のようなフレーズを生成して下さい。この子供は争いのような言葉が好きで、レスバにも意欲的です。
-    以下が掲示板のメッセージです。掲示板のメッセージは「名前:メッセージ/」のように「/」で区切って記述していて、下にあるものほど最新のコメントになっています。`;
-    const seed = Math.floor(Math.random() * 1000000) + 1;
-    const res = await axios.get(`https://text.pollinations.ai/${encodeURIComponent(`${prompt}「${textData}」`)}?model=llama&seed=${seed}`)
-    let text = res.data;
-    
-    text = text.replace(/\*/g, "");
-    text = text.replace(/和歌:/g, "");
-    text = text.replace(/わか:/g, "");
-    text = text.replace(/\//g, "");
     
     await messageedit.sendchatwork(`[rp aid=${accountId} to=${roomId}-${messageId}][pname:${accountId}]さん\n${text}`, roomId);
   } catch (error) {
